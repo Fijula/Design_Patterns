ac37f874fd3155a55c00c9e8fbdb685f599cb060
fijula <fijulaazhikoden123@gmail.com>
2024-04-30 07:29:01 +0530

    ADD : Files for Bridge Design pattern

---
+++ Bridge/HDProcessor.java
@@ -0,0 +1,8 @@
+package Design_Patterns.Bridge;
+
+public class HDProcessor implements VideoProcessor {
+    @Override
+    public void process(String videoFile) {
+        //Process
+    }
+}
---
+++ Bridge/VideoProcessor.java
@@ -0,0 +1,5 @@
+package Design_Patterns.Bridge;
+
+public interface VideoProcessor {
+    void process(String videoFile);
+}
